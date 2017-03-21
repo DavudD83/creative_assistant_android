cf4d6629dbb6cd2cf93c593813fd6e2780e75985
Sergey Kostyuk <contact@s-kostyuk.me>
2017-03-21 23:59:10 +0200

    Fixed test too

--- app/src/androidTest/java/space/dotcat/assistant/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/space/dotcat/assistant/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("space.dotcat.CreativeAssistant", appContext.getPackageName());
+        assertEquals("space.dotcat.assistant", appContext.getPackageName());
     }
 }
